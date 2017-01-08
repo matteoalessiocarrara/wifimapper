9997195a15b9137e8e427b35b2158d2c931cd23a
Matteo Alessio Carrara <sw.matteoac@gmail.com>
2017-01-08 13:10:20 +0100

    Prima commit

---
+++ app/src/main/java/com/carrara/wifimapper/MainActivity.java
@@ -0,0 +1,28 @@
+package com.carrara.wifimapper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
+import android.view.Menu;
+
+
+// TODO Dark theme
+// TODO About
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+}
